880813d5c04c6adfb570af3ae733b24bf936da5d
brassta <brassta@gmail.com>
2019-01-27 13:16:11 +0100

    calendar component logic

--- src/views/Calendar/Calendar.vue
+++ src/views/Calendar/Calendar.vue
@@ -1,21 +1,49 @@
 <template>
     <div class="calendar">
+        <div class="months-nav">
+            <div class="months-toolbar">
+                <button class="prevMonth" v-on:click="prevMonth">Prev</button>
+                <div class="current">{{months[activeIndex].monthName}} : {{months[activeIndex].year}}</div>
+                <button class="nextMonth" v-on:click="nextMonth">Next</button>
+            </div>
+        </div>
+        <div class="months-container" v-for="(monthObject,index) of months">
 
+            <Month v-show="index === activeIndex" :month="{monthObject}" :index="{index}"/>
+        </div>
     </div>
 </template>
 
 <script>
+    import Month from "./Month/Month";
+
     export default {
-        name : "Calendar",
-        props: ['events'],
+        name      : "Calendar",
+        components: { Month },
+        props     : ['events'],
         data() {
             return {
-                months: []
+                months     : [],
+                activeIndex: Number.MIN_SAFE_INTEGER
+            }
+        },
+        methods   : {
+            nextMonth: function(event) {
+                if(this.activeIndex !== this.months.length-2){
+                    this.activeIndex++
+                }
+            },
+            prevMonth: function(event) {
+                if(this.activeIndex!==0){
+                    this.activeIndex--;
+                }
+
             }
         },
         created() {
             let topDate = Number.MIN_SAFE_INTEGER;
             let lastDate = Number.MAX_SAFE_INTEGER;
+            let monthCounter = 0;
             for(let date of this.events.events) {
                 if(date.start > topDate){
                     topDate = date.start
@@ -30,20 +58,29 @@
                 for(let j = 0; j < 12; j++) {
                     const date = new Date(startYear + i - startYear, j, 1);
                     const month = {
-                        monthName: date.toLocaleDateString('en-us', { month: 'long' }),
-                        year: startYear + i - startYear
+                        monthName : date.toLocaleDateString('en-us', { month: 'long' }),
+                        monthIndex: date.getMonth(),
+                        year      : startYear + i - startYear
                     }
-                    console.log(month);
                     this.months.push(month);
-
+                    //## is currentMonth and set currentMonth index
+                    const now = new Date().getTime();
+                    const monthBeginAt = new Date(date.getFullYear(), j, 1).getTime();
+                    const monthEndsAt = new Date(date.getFullYear(), j + 1, 0).getTime();
+                    if(now > monthBeginAt && now < monthEndsAt){
+                        this.activeIndex = monthCounter;
+                    }
+                    monthCounter++;
                 }
             }
-            console.log(this.months);
-
+        },
+        computed  : {
+            isActiveMonth() {
+                console.log(this);
+                return true;
+            }
         }
+
     }
 </script>
 
-<style scoped>
-
-</style>
--- src/views/Calendar/Month/Month.vue
+++ src/views/Calendar/Month/Month.vue
@@ -1,10 +1,27 @@
 <template>
-    
+    <div class="month-inner" v-if="shownMonth" v-bind:data-index="index.index">
+        <h3>{{month.monthObject.monthName}}::{{month.monthObject.year}}</h3>
+        <div class="month-data"></div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Month"
+        name : "Month",
+        props: ['month','index'],
+        data(){
+            return{
+
+            }
+        },
+        computed:{
+            shownMonth:function(){
+               return true;
+            }
+        },
+        created(){
+            // console.log('check this',this.month.monthObject.monthName);
+        }
     }
 </script>
 
